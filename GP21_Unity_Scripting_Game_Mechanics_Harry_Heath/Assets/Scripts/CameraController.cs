bd3936800cf40e6388cb9ad1fb383458669ada5b
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-24 20:02:12 +0100

    CameraController.cs: Updated variable names and clean-up

--- GP21_Unity_Scripting_Game_Mechanics_Harry_Heath/Assets/Scripts/CameraController.cs
+++ GP21_Unity_Scripting_Game_Mechanics_Harry_Heath/Assets/Scripts/CameraController.cs
@@ -8,37 +8,36 @@ public class CameraController : MonoBehaviour {
     public float damping = 10f;
  
     // camera level boundaries
-    public float mapX = 300.0f;
-    public float mapY = 300.0f;
+    public float mapXBoundary = 300.0f;
+    public float mapYBoundary = 300.0f;
 
     private Vector3 wantedPosition;
-  
-  
+    
     void Awake() 
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
  
-    void LateUpdate() {
-
+    void LateUpdate() 
+    {
         // check if in bounds on X plane
-        wantedPosition.x = (wantedPosition.x < mapX) ? mapX : wantedPosition.x;
-        wantedPosition.x = (wantedPosition.x > mapX) ? mapX : wantedPosition.x;
+        wantedPosition.x = (wantedPosition.x < mapXBoundary) ? mapXBoundary : wantedPosition.x;
+        wantedPosition.x = (wantedPosition.x > mapXBoundary) ? mapXBoundary : wantedPosition.x;
  
         // check if in bounds on  Y plane
-        wantedPosition.y = (wantedPosition.y < mapY) ? mapY : wantedPosition.y;
-        wantedPosition.y = (wantedPosition.y > mapY) ? mapY : wantedPosition.y;
+        wantedPosition.y = (wantedPosition.y < mapYBoundary) ? mapYBoundary : wantedPosition.y;
+        wantedPosition.y = (wantedPosition.y > mapYBoundary) ? mapYBoundary : wantedPosition.y;
         
         // keeps camera at max bounds X point if beyond it
-        if (wantedPosition.x > mapX)
+        if (wantedPosition.x > mapXBoundary)
         {
-            wantedPosition.x = mapX;
+            wantedPosition.x = mapXBoundary;
         }
         
         // keeps camera at max bounds Y point if beyond it
-        else if (wantedPosition.y > mapY)
+        else if (wantedPosition.y > mapYBoundary)
         {
-            wantedPosition.y = mapY;
+            wantedPosition.y = mapYBoundary;
         }
         
         // follow Player if not out of bounds
